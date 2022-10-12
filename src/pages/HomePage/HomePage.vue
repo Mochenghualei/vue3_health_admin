c4c9ca00f03201ba7d9aae3d95e4a6c6d82b387a
Mochenghualei <18830279823@163.com>
2022-10-12 17:59:21 +0800

    添加蒙版

--- src/pages/HomePage/HomePage.vue
+++ src/pages/HomePage/HomePage.vue
@@ -1,22 +1,26 @@
 <template>
     <div class="homepage_container">
+        <!-- 初始蒙版 -->
+        <div class="mask" v-if="startStyleShow">
+            <h1 @click.once="startStyleShow = false">Hello,{{ username }}</h1>
+        </div>
         <!-- 侧边栏 -->
-        <div class="home_aside">
+        <div class="home_aside" v-if="!startStyleShow">
             <button @click="visible = true" class="showDrawnBtn">></button>
         </div>
 
         <!-- 抽屉 -->
-        <MainDrawer v-model:visible="visible"></MainDrawer>
+        <MainDrawer v-model:visible="visible" v-if="!startStyleShow"></MainDrawer>
 
         <!-- 主体区域 -->
-        <div class="home_main">
+        <div class="home_main" v-if="!startStyleShow">
             <keep-alive>
                 <component :is="currentTab"></component>
             </keep-alive>
         </div>
 
         <!-- 右侧区域 -->
-        <div class="home_right">
+        <div class="home_right" v-if="!startStyleShow">
             <!-- 播放器 -->
             <div class="audio_container" v-once>
                 <BaseAudio />
@@ -50,6 +54,7 @@
 import { ref, reactive, defineAsyncComponent, shallowRef } from "vue"
 import { useHomePageStore } from "store/homePage"
 import MainDrawer from "./components/HomePageDrawer.vue"
+import { getUserInfo } from "utils/userInfo"
 const BaseAudio = defineAsyncComponent(() => import("components/BaseAudio.vue"))
 const BaseClock = defineAsyncComponent(() => import("components/BaseClock.vue"))
 const BaseSwitchTabs = defineAsyncComponent(() =>
@@ -61,9 +66,12 @@ const BaseWeather = defineAsyncComponent(() =>
 const baseKpi = defineAsyncComponent(() => import("components/BaseKpi.vue"))
 
 const visible = ref(false)
+// 蒙版按钮
+const startStyleShow = ref(true)
 let currentTab = shallowRef(null)
 const homePageStore = useHomePageStore()
 const data = homePageStore.data
+const username = getUserInfo()
 
 const handleSwitchTab = (tab) => {
     currentTab.value = tab
