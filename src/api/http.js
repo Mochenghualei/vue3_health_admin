fbfad3d7bf86accdb7466789d8790d86c34aea0f
Mochenghualei <18830279823@163.com>
2022-10-20 09:46:11 +0800

    添加错误处理程序

--- src/api/http.js
+++ src/api/http.js
@@ -7,7 +7,7 @@ const requests = axios.create({
     baseURL: "http://localhost:5173",
     timeout: 8000,
 })
-
+// 请求拦截器
 requests.interceptors.request.use(
     (config) => {
         nprogress.start()
@@ -18,7 +18,7 @@ requests.interceptors.request.use(
         Promise.reject(new Error(error))
     }
 )
-
+// 响应拦截器
 requests.interceptors.response.use(
     (res) => {
         nprogress.done()
@@ -26,8 +26,35 @@ requests.interceptors.response.use(
     },
     (error) => {
         nprogress.done()
-        Promise.reject(new Error(error))
+        // 存在response说明服务器有响应
+        if (error.response) {
+            let response = error.response
+            if (response.status >= 400) {
+                // 服务器异常
+                handleError(response, 1)
+            } else {
+                handleError(null, 2)
+            }
+        }
+        return Promise.reject(new Error(error))
     }
 )
+
+// 错误处理程序
+function handleError(error, type) {
+    if (type == 1) {
+        // 处理接口异常，从config字段中获取请求信息
+        let { url, method, params, data } = error.config
+        let errData = {
+            url,
+            method,
+            params: { query: params, body: data },
+            error: error.data.message || JSON.stringify(error.data),
+        }
+        console.log("errData===", errData)
+    } else {
+        // ...
+    }
+}
 
 export default requests
--- src/main.js
+++ src/main.js
@@ -22,6 +22,24 @@ app.provide("message", message)
 // 全局注册echarts
 app.config.globalProperties.$echarts = echarts
 
+// 错误信息捕获
+app.config.errorHandler = (err, instance, info) => {
+    let errData
+    if (err instanceof Error) {
+        let { name, message } = err
+        errData = {
+            type: name,
+            error: message,
+        }
+    } else {
+        errData = {
+            type: "other",
+            error: JSON.stringify(err),
+        }
+    }
+    console.log("errData===", errData)
+}
+
 app.use(router).use(createPinia()).use(Antd).mount("#app")
 
 // 循环使用全部全部图标
